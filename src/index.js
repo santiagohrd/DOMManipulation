253b84b911e644e29b5abfabc00d4269a8316242
SantiagoHrd <santiagohrd1@gmail.com>
2024-03-26 17:57:14 -0500

    Create asyn function to fetch data, and create a funtion to dispay each item

--- src/index.js
+++ src/index.js
@@ -3,4 +3,29 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = 'https://platzi-avo.vercel.app/api/avo'
+
+async function fetchData() {
+    const response = await fetch(url);
+    const info = await response.json();
+    displayItems(info.data);
+}
+fetchData();
+
+function displayItems(item){
+    const allItems = [];
+    item.forEach((element) =>{
+        const imagen = document.createElement('img');
+
+        const titulo = document.createElement('h1');
+        
+        const precio = document.createElement('div');
+
+        const container = document.createElement('div');
+        container.append(imagen, titulo, precio);
+
+        allItems.push(container);
+    }
+    );
+    document.body.append(...allItems);
+}
